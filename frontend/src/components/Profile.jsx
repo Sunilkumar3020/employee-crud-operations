bf5ec1785581a4a80caefef11dd6615ab3238960
Sunil kumar <sunilkumar3020@gmail.com>
2026-05-05 12:44:42 +0530

    fields editing

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,8 +1,28 @@
 import { useState } from "react"
 
 export default function Profile({ profile }) {
-    const [] = useState()
+    const [isEditing, setIsEditing] = useState(false)
+    const [editingFields, setEditingFields] = useState({
+        name: profile.name,
+        age: profile.age,
+        email: profile.email,
+        phone: profile.phone,
+        department: profile.department
+    })
 
+    const handleEdit = (e) => {
+        setIsEditing(prevState => !prevState)
+    }
+    const handleSave = () => { }
+    const handleInputChange = (e) => {
+        console.log(e)
+        setEditingFields(previousValue => (
+            {
+                ...previousValue,
+                [e.target.name]: e.target.value
+            }
+        ))
+    }
     return (
         <>
             <div className="border border-gray-300  p-4">
@@ -12,18 +32,36 @@ export default function Profile({ profile }) {
                     <img src={profile.photo} alt="" />
 
                 </div>
-                    <button className="absolute top-0 right-0 cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    {isEditing ? (<button onClick={handleSave} className="absolute top-0 right-0 cursor-pointer"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-device-floppy">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M16 3a1 1 0 0 1 .707 .293l4 4a1 1 0 0 1 .293 .707v10a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3v-12a3 3 0 0 1 3 -3h1v4a1 1 0 0 0 .883 .993l.117 .007h6a1 1 0 0 0 1 -1v-4zm-4 8a2.995 2.995 0 0 0 -2.995 2.898a1 1 0 0 0 -.005 .102a3 3 0 1 0 3 -3m1 -8v3h-4v-3z" />
+                    </svg>
+                    </button>) : (<button onClick={handleEdit} className="absolute top-0 right-0 cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
                     </svg>
-                    </button></div>
-                <h3 className="border-b border-gray-200 p-2">Employee Name: {profile.name}</h3>
-                <p className="border-b border-gray-200 p-2">Age: {profile.age}</p>
-                <p className="border-b border-gray-200 p-2">Email: {profile.email}</p>
-                <p className="border-b border-gray-200 p-2">Phone: {profile.phone}</p>
-                <p className="  p-2">Department: {profile.department}</p>
+                    </button>)}
 
 
-            </div>
+                </div>
+                {isEditing ? (<>
+                    <form className="flex flex-col">
+                        <input type="text" className="border-b border-gray-200 p-2" value={editingFields.name} name="name" onChange={(e) => handleInputChange(e)} placeholder="Enter your name" />
+                        <input type="text" className="border-b border-gray-200 p-2" value={editingFields.age} name="age" onChange={(e) => handleInputChange(e)} placeholder="Enter your age" />
+                        <input type="email" className="border-b border-gray-200 p-2" value={editingFields.email} name="email" onChange={(e) => handleInputChange(e)} placeholder="Enter your email" />
+                        <input type="text" className="border-b border-gray-200 p-2" value={editingFields.phone} name="phone" onChange={(e) => handleInputChange(e)} placeholder="Enter your phone number" />
+                        <input type="text" className="border-b border-gray-200 p-2" value={editingFields.department} name="department" onChange={(e) => handleInputChange(e)} placeholder="Enter your department" />
+                    </form>
+
+                </>) : (<>
+                    <h3 className="border-b border-gray-200 p-2">Employee Name : {profile.name}</h3>
+                    <p className="border-b border-gray-200 p-2">Age: {profile.age}</p>
+                    <p className="border-b border-gray-200 p-2">Email: {profile.email}</p>
+                    <p className="border-b border-gray-200 p-2">Phone: {profile.phone}</p>
+                    <p className="  p-2">Department: {profile.department}</p>
+                </>)}
+
+
+            </div >
         </>
     )
 }
