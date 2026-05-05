a471a7d16c73a9e2db39e8432f395024ffb00b54
Sunil kumar <sunilkumar3020@gmail.com>
2026-05-05 10:18:31 +0530

    add loading and error handling in backend

--- frontend/src/components/ProfileGrid.jsx
+++ frontend/src/components/ProfileGrid.jsx
@@ -5,19 +5,27 @@ import Profile from "./Profile"
 
 export default function ProfileGrid() {
     const [employees, setEmployees] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [errors, setErrors] = useState(null)
     useEffect(() => {
         const fetchEmployee = async () => {
             try {
+                setLoading(true)
                 const response = await API.get('/employee')
+                setLoading(false)
                 console.log(response.data.data)
                 setEmployees(response.data.data)
             } catch (error) {
                 console.error(error)
+                setErrors(error.message)
             }
         }
 
         fetchEmployee()
     }, [])
+
+    if (loading) <p>Loading ...</p>
+    if (error) <p>{error.message}</p>
     return (
         <>
             <h2>Employees Details</h2>
